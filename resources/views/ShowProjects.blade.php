f14b6dd0d21d2bf7ad75f8c4f6a2f55d3a2c76a1
CristJoenathan24 <crist.joenathan8@gmail.com>
2020-08-09 22:25:10 +0700

    adding JS

--- resources/views/ShowProjects.blade.php
+++ resources/views/ShowProjects.blade.php
@@ -57,3 +57,14 @@
      </div>
  </div>
 @endsection
+
+@push('scripts')
+    <script>
+        Swal.fire({
+            title: 'Berhasil!',
+            text: 'Memasangkan script sweet alert',
+            icon: 'success',
+            confirmButtonText: 'Cool'
+        })
+    </script>
+@endpush
